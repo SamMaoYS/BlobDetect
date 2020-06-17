5ba7ec9bad17c3ca51c85a9316f3423a21b35e96
sam <sammaoys@gmail.com>
2020-06-16 23:52:28 -0700

    add LoG filtering with signal normalization

--- src/main.py
+++ src/main.py
@@ -4,15 +4,67 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 
-def gaussian_kernel(g_sigma, g_size):
-    if g_size % 2 == 0:
-        g_size += 1
-    idx_range = np.linspace(-(g_size - 1) / 2., (g_size - 1) / 2., g_size)
+def gaussian_kernel(sigma, size):
+    if size % 2 == 0:
+        size += 1
+    idx_range = np.linspace(-(size - 1) / 2., (size - 1) / 2., size)
     x_idx, y_idx = np.meshgrid(idx_range, idx_range)
-    tmp_cal = 1. / (2. * g_sigma ** 2)
-    kernel = np.exp(-(np.square(x_idx) + np.square(y_idx)) * tmp_cal)
-    kernel /= np.sum(kernel)
+    tmp_cal = -(np.square(x_idx) + np.square(y_idx)) / (2. * sigma ** 2)
+    kernel = np.exp(tmp_cal)
+    kernel[kernel < np.finfo(float).eps * np.amax(kernel)] = 0
+    k_sum = np.sum(kernel)
+    if k_sum != 0:
+        kernel /= np.sum(kernel)
     return kernel
+
+
+def log_kernel(sigma, size):
+    if size % 2 == 0:
+        size += 1
+    sigma2 = sigma ** 2
+    idx_range = np.linspace(-(size - 1) / 2., (size - 1) / 2., size)
+    x_idx, y_idx = np.meshgrid(idx_range, idx_range)
+    tmp_cal = -(np.square(x_idx) + np.square(y_idx)) / (2. * sigma2)
+    kernel = np.exp(tmp_cal)
+    kernel[kernel < np.finfo(float).eps * np.amax(kernel)] = 0
+    k_sum = np.sum(kernel)
+    if k_sum != 0:
+        kernel /= np.sum(kernel)
+    tmp_kernel = np.multiply(kernel, np.square(x_idx) + np.square(y_idx) - 2 * sigma2) / (sigma2 ** 2)
+    kernel = tmp_kernel - np.sum(tmp_kernel) / (size ** 2)
+    return kernel
+
+
+def show_kernel(kernel):
+    size = np.shape(kernel)[0]
+    idx_range = np.linspace(-(size - 1) / 2., (size - 1) / 2., size)
+    x, y = np.meshgrid(idx_range, idx_range)
+
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(x, y, kernel, rstride=1, cstride=1,
+                    cmap='viridis', edgecolor='none', antialiased=True)
+    ax.view_init(15, 0)
+    plt.show()
+    plt.cla()
+    plt.clf()
+    plt.close(fig)
+
+
+def non_max_suppression(img):
+    h, w = np.shape(img)
+    result = np.zeros_like(img)
+    # TODO: Improve performance
+    for i in range(1, h-2):
+        for j in range(1, w-2):
+            if img[i, j] > 0:
+                window = np.array([img[i-1, j-1], img[i-1, j], img[i-1, j+1],
+                                   img[i, j-1], img[i, j], img[i, j+1],
+                                   img[i+1, j-1], img[i+1, j], img[i+1, j+1]])
+                if np.max(window) == img[i, j]:
+                    result[i, j] = img[i, j]
+
+    return result
 
 
 def main():
@@ -20,24 +72,16 @@ def main():
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = np.float32(gray)
 
-    sigma = 2
-    size = 31
-    kernel = gaussian_kernel(sigma, size)
-    print(kernel)
-
-    idx_range = np.linspace(-(99 - 1) / 2., (99 - 1) / 2., 99)
-    x, y = np.meshgrid(idx_range, idx_range)
-    z = np.pad(kernel, ((49-15, 49-15), (49-15, 49-15)), 'constant')
-
-    fig = plt.figure(figsize=(12, 6))
-    ax = fig.gca(projection='3d')
-    ax.plot_surface(x, y, -z, rstride=1, cstride=1,
-                    cmap='viridis', edgecolor='none', antialiased=True)
-
-    ax.set_zlim(-0.1, 0)
-    ax.set_zticks(np.linspace(-0.1, 0, 5))
-    plt.show()
-    plt.close(fig)
+    sigma0 = np.sqrt(2)
+    k = np.sqrt(2)
+    num_scales = 10
+    sigmas = sigma0*np.power(k, np.arange(num_scales))
+    for i in range(num_scales):
+        size = np.int(2*np.ceil(4*sigmas[i])+1)
+        kernel = log_kernel(sigmas[i], size)*np.power(sigmas[i], 2)
+        filtered = cv2.filter2D(gray, cv2.CV_32F, kernel)
+        filtered = pow(filtered, 2)
+        filtered = non_max_suppression(filtered)
 
 
 if __name__ == "__main__":
